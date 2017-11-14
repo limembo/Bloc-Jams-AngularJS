351e8cb58c2ae16a9acf8421e4e27ebb329fa1f0
limembo <lmembis@gmail.com>
2017-11-14 06:30:47 -0500

    Fixed errors in app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@
      $stateProvider
          .state('landing', {
              url: '/',
-             controller: 'LandingCtrl as landing'
+             controller: 'LandingCtrl as landing',
              templateUrl: '/templates/landing.html'
            })
            .state('album', {
@@ -19,7 +19,7 @@
            })
            .state('collection', {
                url: '/collection',
-               controller: 'CollectionCtrl as collection'
+               controller: 'CollectionCtrl as collection',
                templateUrl: '/templates/collection.html'
            });
 }
