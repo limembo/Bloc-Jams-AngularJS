83843c39ebc4ac1bdc3ff51949119160e585a5a3
limembo <lmembis@gmail.com>
2017-11-24 17:17:42 -0500

    Added functionality to seek bar

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-  function SongPlayer(Fixtures) {
+  function SongPlayer($rootScope, Fixtures) {
     var SongPlayer = {};
 
 /**
@@ -31,6 +31,12 @@
         preload: true
     });
 
+    currentBuzzObject.bind('timeupdate', function() {
+         $rootScope.$apply(function() {
+             SongPlayer.currentTime = currentBuzzObject.getTime();
+         });
+     });
+
     SongPlayer.currentSong = song;
  };
 
@@ -61,6 +67,12 @@
   */
 
 SongPlayer.currentSong = null;
+
+/**
+* @desc Current playback time (in seconds) of currently playing song
+* @type {Number}
+*/
+SongPlayer.currentTime = null;
 
 SongPlayer.play = function(song) {
    song = song || SongPlayer.currentSong;
@@ -104,10 +116,22 @@ SongPlayer.previous = function() {
           playSong(song);
       }
   };
+
+  /**
+* @function setCurrentTime
+* @desc Set current time (in seconds) of currently playing song
+* @param {Number} time
+*/
+SongPlayer.setCurrentTime = function(time) {
+    if (currentBuzzObject) {
+        currentBuzzObject.setTime(time);
+    }
+};
+
     return SongPlayer;
   }
 
   angular
     .module('blocJams')
-    .factory('SongPlayer', ['Fixtures', SongPlayer]);
+    .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
